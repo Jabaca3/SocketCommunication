b9fe07453451567ed1e6ab4701b2a4c0aa768562
Joseph <Jojobaca123@gmail.com>
2018-10-12 17:48:43 -0600

    Update NetworkClient.java

--- NetworkClient.java
+++ NetworkClient.java
@@ -9,7 +9,7 @@ import java.net.UnknownHostException;
 public class NetworkClient {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		Socket socket = new Socket("192.168.0.32",9000);
+		Socket socket = new Socket("Enter IP here",9000);
 		DataInputStream in  = new DataInputStream(socket.getInputStream());
 		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
 		out.writeUTF("Hello this is Client");
