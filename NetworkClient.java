ebadf966ca4ce8787336a8428960f50760780410
Joseph <Jojobaca123@gmail.com>
2018-10-12 17:50:40 -0600

    Add files via upload

---
+++ NetworkClient.java
@@ -0,0 +1,21 @@
+package test;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class NetworkClient {
+
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		
+		Socket socket = new Socket("Enter Ip here",9000);
+		DataInputStream in  = new DataInputStream(socket.getInputStream());
+		DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+		
+		out.writeUTF("Hello this is Client");
+
+	}
+
+}
